288b2c218b29b12ccb8c59f851a82266058c707a
Lorenzo D'Elia <lorenzodelia@MBP16diLorenzo.homenet.telecomitalia.it>
2024-04-14 14:56:20 +0200

    first dev release

--- dist-dev/utils/inputs/getProfileRpcPath.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getProfileRpcPath = void 0;
-const constants_1 = require("../../common/constants");
-const getProfileRpcPath = (profile) => constants_1.rpcPaths[profile];
-exports.getProfileRpcPath = getProfileRpcPath;
